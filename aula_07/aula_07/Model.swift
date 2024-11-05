3e14f846fca33618c4dbfa536022bcabd3b77333
Anderson Henrique <144293720+anderson-henriqq@users.noreply.github.com>
2024-11-04 21:38:35 -0300

    Add files via upload

---
+++ aula_07/aula_07/Model.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+struct HaPo: Codable, Identifiable {
+    let id: String
+    let name: String?
+    let alternate_names: [String]?
+    let species: String?
+    let gender: String?
+    let house: String?
+    let dateOfBirth: String?
+    let yearOfBirth: String?
+    let wizard: Bool?
+    let ancestry: String?
+    let eyeColour: String?
+    let hairColour: String?
+    let wand: Wand
+    let patronus: String?
+    let hogwartsStudents: Bool?
+    let hogwartsStaff: Bool?
+    let actor: String?
+    let alternate_actors: [String]?
+    let alive: Bool?
+    let image: String?
+}
+
+struct Wand: Codable{
+    let wood: String?
+    let core: String?
+    let length: Double?
+}
---
+++ aula_07/aula_07/aula_07App.swift
@@ -0,0 +1,17 @@
+//
+//  aula_07App.swift
+//  aula_07
+//
+//  Created by Turma02-1 on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct aula_07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
